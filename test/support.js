14c79bf2dc60f02e46279c7262be43a38419667e
Batuhan Öztürk <batuhanoztrk@hotmail.com>
2022-01-30 16:32:18 +0300

    Add initial support for Sequelize V6

--- test/support.js
+++ test/support.js
@@ -1,24 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs'),
-	path = require('path'),
-	_ = require('lodash'),
-	Sequelize = require('sequelize'),
-	DataTypes = require('sequelize/lib/data-types'),
-	chai = require('chai'),
-	{expect} = chai,
-	chaiAsPromised = require('chai-as-promised'),
-	Config = require('./config/config');
+const fs = require("fs"),
+	path = require("path"),
+	_ = require("lodash"),
+	bluebird = require("bluebird"),
+	Sequelize = require("sequelize"),
+	DataTypes = require("sequelize/lib/data-types"),
+	chai = require("chai"),
+	{ expect } = chai,
+	chaiAsPromised = require("chai-as-promised"),
+	Config = require("./config/config");
 
-require('../lib/index')(Sequelize);
+require("../lib/index")(Sequelize);
 
 chai.use(chaiAsPromised);
 
 // Make sure errors get thrown when testing
-Sequelize.Promise.onPossiblyUnhandledRejection((e) => {
+bluebird.Promise.onPossiblyUnhandledRejection((e) => {
 	throw e;
 });
-Sequelize.Promise.longStackTraces();
+bluebird.Promise.longStackTraces();
 
 const Support = {
 	Sequelize,
@@ -41,35 +42,46 @@ const Support = {
 		});
 	},
 
-	prepareTransactionTest(sequelize, callback) { // eslint-disable-line consistent-return
+	prepareTransactionTest(sequelize, callback) {
+		// eslint-disable-line consistent-return
 		const dialect = Support.getTestDialect();
 
-		if (dialect === 'sqlite') {
-			const p = path.join(__dirname, 'tmp', 'db.sqlite');
+		if (dialect === "sqlite") {
+			const p = path.join(__dirname, "tmp", "db.sqlite");
 
-			return new Sequelize.Promise(((resolve) => {
+			return new bluebird.Promise((resolve) => {
 				// We cannot promisify exists, since exists does not follow node callback convention -
 				// first argument is a boolean, not an error / null
 				if (fs.existsSync(p)) {
-					resolve(Sequelize.Promise.promisify(fs.unlink)(p));
+					resolve(bluebird.Promise.promisify(fs.unlink)(p));
 				} else {
 					resolve();
 				}
-			})).then(() => { // eslint-disable-line consistent-return
-				const options = Object.assign({}, sequelize.options, {storage: p}),
-					_sequelize = new Sequelize(sequelize.config.database, null, null, options);
+			}).then(() => {
+				// eslint-disable-line consistent-return
+				const options = Object.assign({}, sequelize.options, {
+						storage: p,
+					}),
+					_sequelize = new Sequelize(
+						sequelize.config.database,
+						null,
+						null,
+						options
+					);
 
 				if (callback) {
-					_sequelize.sync({force: true}).success(() => { callback(_sequelize); });
+					_sequelize.sync({ force: true }).success(() => {
+						callback(_sequelize);
+					});
 				} else {
-					return _sequelize.sync({force: true}).return(_sequelize);
+					return _sequelize.sync({ force: true }).return(_sequelize);
 				}
 			});
 		}
 		if (callback) {
 			callback(sequelize);
 		} else {
-			return Sequelize.Promise.resolve(sequelize);
+			return bluebird.Promise.resolve(sequelize);
 		}
 	},
 
@@ -81,14 +93,14 @@ const Support = {
 
 		const sequelizeOptions = _.defaults(options, {
 			host: options.host || config.host,
-			logging: (process.env.SEQ_LOG ? console.log : false), // eslint-disable-line no-console
+			logging: process.env.SEQ_LOG ? console.log : false, // eslint-disable-line no-console
 			dialect: options.dialect,
 			port: options.port || process.env.SEQ_PORT || config.port,
 			pool: config.pool,
-			dialectOptions: options.dialectOptions || {}
+			dialectOptions: options.dialectOptions || {},
 		});
 
-		if (process.env.DIALECT === 'postgres-native') {
+		if (process.env.DIALECT === "postgres-native") {
 			sequelizeOptions.native = true;
 		}
 
@@ -97,7 +109,10 @@ const Support = {
 		}
 
 		return this.getSequelizeInstance(
-			config.database, config.username, config.password, sequelizeOptions
+			config.database,
+			config.username,
+			config.password,
+			sequelizeOptions
 		);
 	},
 
@@ -108,21 +123,28 @@ const Support = {
 	},
 
 	clearDatabase(sequelize) {
-		return sequelize
-			.drop()
-			.then(() => {
-				if (sequelize.modelManager.daos) sequelize.modelManager.daos = [];
-				if (sequelize.modelManager.models) sequelize.modelManager.models = [];
-				sequelize.models = {};
+		return sequelize.drop().then(() => {
+			if (sequelize.modelManager.daos) sequelize.modelManager.daos = [];
+			if (sequelize.modelManager.models)
+				sequelize.modelManager.models = [];
+			sequelize.models = {};
 
-				return sequelize
-					.getQueryInterface()
-					.dropAllEnums();
-			});
+			const dialect = Support.getTestDialect();
+
+			if (dialect === "postgres") {
+				return sequelize.getQueryInterface().dropAllEnums();
+			}
+		});
 	},
 
 	getSupportedDialects() {
-		return fs.readdirSync(`${__dirname}/../node_modules/sequelize/lib/dialects`).filter(file => ((file.indexOf('.js') === -1) && (file.indexOf('abstract') === -1)));
+		return fs
+			.readdirSync(`${__dirname}/../node_modules/sequelize/lib/dialects`)
+			.filter(
+				(file) =>
+					file.indexOf(".js") === -1 &&
+					file.indexOf("abstract") === -1
+			);
 	},
 
 	checkMatchForDialects(dialect, value, expectations) {
@@ -134,36 +156,38 @@ const Support = {
 	},
 
 	getTestDialect() {
-		let envDialect = process.env.DIALECT || 'mysql';
+		let envDialect = process.env.DIALECT || "mysql";
 
-		if (envDialect === 'postgres-native') {
-			envDialect = 'postgres';
+		if (envDialect === "postgres-native") {
+			envDialect = "postgres";
 		}
 
 		if (this.getSupportedDialects().indexOf(envDialect) === -1) {
-			throw new Error(`The dialect you have passed is unknown. Did you really mean: ${envDialect}`);
+			throw new Error(
+				`The dialect you have passed is unknown. Did you really mean: ${envDialect}`
+			);
 		}
 
 		return envDialect;
 	},
 
 	dialectIsMySQL(strict) {
-		const envDialect = process.env.DIALECT || 'mysql';
+		const envDialect = process.env.DIALECT || "mysql";
 		if (strict === undefined) {
 			strict = false;
 		}
 
 		if (strict) {
-			return envDialect === 'mysql';
+			return envDialect === "mysql";
 		}
-		return ['mysql', 'mariadb'].indexOf(envDialect) !== -1;
+		return ["mysql", "mariadb"].indexOf(envDialect) !== -1;
 	},
 
 	getTestDialectTeaser(moduleName) {
 		let dialect = this.getTestDialect();
 
-		if (process.env.DIALECT === 'postgres-native') {
-			dialect = 'postgres-native';
+		if (process.env.DIALECT === "postgres-native") {
+			dialect = "postgres-native";
 		}
 
 		return `[${dialect.toUpperCase()}] ${moduleName}`;
@@ -173,7 +197,7 @@ const Support = {
 		let url;
 		const dbConfig = config[config.dialect];
 
-		if (config.dialect === 'sqlite') {
+		if (config.dialect === "sqlite") {
 			url = `sqlite://${dbConfig.storage}`;
 		} else {
 			let credentials = dbConfig.username;
@@ -189,7 +213,7 @@ const Support = {
 	expectsql(query, expectations) {
 		let expectation = expectations[Support.sequelize.dialect.name];
 
-		if (!expectation && Support.sequelize.dialect.name === 'mariadb') {
+		if (!expectation && Support.sequelize.dialect.name === "mariadb") {
 			expectation = expectations.mysql;
 		}
 
@@ -200,10 +224,10 @@ const Support = {
 		}
 
 		expect(query).to.equal(expectation);
-	}
+	},
 };
 
-beforeEach(function() {
+beforeEach(function () {
 	this.sequelize = Support.sequelize; // eslint-disable-line no-invalid-this
 });
 
